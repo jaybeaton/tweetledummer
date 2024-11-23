c144078d5e9b10053a0cd143d5136e1edda0e988
Jay Beaton <jrb@jaybeaton.com>
2024-11-23 01:06:40 -0500

    rearrange info bar

--- tweetledummer/index.php
+++ tweetledummer/index.php
@@ -31,24 +31,30 @@ if (!empty($profile->avatar)) {
 <body class="read">
 
 <div class="info-bar">
-<?php
-  if (!empty($profile_img)) {
-    print $profile_img;
-  }
-?>
-    <div>
-        <a href="/bulk.php"><img src="images/list.svg" width="30" height="30" alt="Bulk" title="Bulk"></a>
-    </div>
-    <div>
-        <span id="unread-count">0</span>
-    </div>
-    <div>
-        <span id="current-view"></span>
-    </div>
-    <div class="tweetledum-controls" style="display: none;">
-        <button class="tweetledum-controls-up" data-keycode="75">⬆️</button>
-        <button class="tweetledum-controls-open" data-keycode="86">👓</button>
-        <button class="tweetledum-controls-down" data-keycode="74">⬇️</button>
+    <div class="info-bar__inner">
+        <div class="info-bar__items">
+            <div>
+                <span id="unread-count">0</span>
+            </div>
+            <div>
+                <span id="current-view"></span>
+            </div>
+        </div>
+        <div class="info-bar__items">
+            <div>
+                <a href="/bulk.php"><img src="images/list.svg" width="30" height="30" alt="Bulk" title="Bulk"></a>
+            </div>
+            <?php
+            if (!empty($profile_img)) {
+                print $profile_img;
+            }
+            ?>
+        </div>
+        <div class="tweetledum-controls" style="display: none;">
+            <button class="tweetledum-controls-up" data-keycode="75">⬆️</button>
+            <button class="tweetledum-controls-open" data-keycode="86">👓</button>
+            <button class="tweetledum-controls-down" data-keycode="74">⬇️</button>
+        </div>
     </div>
 </div>
 <div class="main">
